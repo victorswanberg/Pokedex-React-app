756f762f93a42c2ff9334dbf4216d9ace1c99b2e
Victor Swanberg <victor.swanberg@sublimemedia.com>
2021-04-29 09:48:28 -0700

    deleted the react svg and component info from react, added in a form for input and started importing the api. Running into issues with pulling the data

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  // Setting my variable fields
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase())
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon()
+  }
+
+  //Getting the api
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      // URL for the poke api using literals to seperate individual data
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      const res = await axios.get(url);
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name);
+      setPokemonData(toArray);
+      console.log(res)
+    } catch (e) {
+      console.log(e)
+    }
+  };
+
+  // useEffect(() => {
+  //   getPokemon();
+  // }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label
+          type="text"
+          onChange={handleChange}
+          placeholder="What is that pokemon?">
+        </label>
+      </form>
     </div>
   );
 }
